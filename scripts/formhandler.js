cf45b889257b5ab2a388b0b0296eb9d89273c621
Akriti Yadav <94akritiyadav@gmail.com>
2021-07-22 12:17:03 +0530

    ch10 prob2 solved

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -11,22 +11,16 @@
     if (this.$formElement.length === 0) {
       throw new Error('Could not find element with selector: ' + selector);
     }
-    // update strengthOutput when slider changes and add color
+
     var slider = document.getElementById("threads");
     var sliderOutput = document.getElementById("threadOp");
     var sliderLabel = document.getElementById('threadc');
 
-    // set initial color
     sliderOutput.style.color = "blue";
     sliderLabel.style.color = "green";
 
-    slider.addEventListener("input", function() { // using change only shows
-                                                  // number after letting go of slider
-      // both value and textContent seem to work
-      // can't find any discussion online about which to use
-      // sliderOutput.textContent = slider.value;
+    slider.addEventListener("input", function() {
       sliderOutput.value = slider.value
-      // change the color of the label and number based on intensity
       var intensityColor;
       if (slider.value < 2) {
         intensityColor = "green";
