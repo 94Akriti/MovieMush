358c53897823d20cc4a11d132eaa53a04013b334
Akriti Yadav <94akritiyadav@gmail.com>
2021-07-22 11:28:46 +0530

    Movie Mush Frontend JQuery

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 (function (window) {
  'use strict';
+ var FORM_SELECTOR = '[data-movie-demand="form"]';
  var App = window.App;
  var portal = App.portal;
  var DataStore = App.DataStore;
+ var FormHandler = App.FormHandler;
  var mymovie = new portal('701', new DataStore());
  window.mymovie = mymovie;
+ var formHandler = new FormHandler(FORM_SELECTOR);
+
+ formHandler.addSubmitHandler(mymovie.createOrder.bind(mymovie));
+ console.log(formHandler);
 })(window);
